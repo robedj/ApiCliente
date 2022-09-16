31cc2fbb635b730d207c6decb9f28c38af22504a
robedj <ed.wassouf@gmail.com>
2022-09-15 23:18:05 -0400

    Criando Bean do modelMappar e retornando

--- src/main/java/br/com/tec/client/ClientApplication.java
+++ src/main/java/br/com/tec/client/ClientApplication.java
@@ -1,11 +1,19 @@
 package br.com.tec.client;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientApplication {
 
+	@Bean
+	public ModelMapper modelMapper(){
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setSkipNullEnabled(true);
+		return modelMapper;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
