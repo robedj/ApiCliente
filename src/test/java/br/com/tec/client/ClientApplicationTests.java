a1d76106ed177b142a82edab89703e988361cf11
robedj <ed.wassouf@gmail.com>
2022-09-15 21:50:28 -0400

    Criação do projeto Api Rest Cliente utilizando SpringBoot

---
+++ src/test/java/br/com/tec/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tec.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
