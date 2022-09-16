d17f2e8757bc14324a1ccc0f8233ddd63bd204bb
robedj <ed.wassouf@gmail.com>
2022-09-15 22:21:24 -0400

    W.IP: Criação da entidade Cliente

---
+++ src/main/java/br/com/tec/client/entity/Cliente.java
@@ -0,0 +1,29 @@
+package br.com.tec.client.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Cliente implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "cpf")
+    private String cpf;
+}
